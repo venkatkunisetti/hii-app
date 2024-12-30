a56d5d0df6dffb9233870672e209e81761a7ff91
venkatkunisetti <venkatkunisetti17@gmail.com>
2024-12-30 12:27:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React,{useState} from "react";
+
+function App() {
+  useState();
+  return (
+    <div className="App">
+      <h1>venkat react</h1>
+    </div>
+  );
+}
+
+export default App;
